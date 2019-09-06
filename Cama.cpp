f038007282ceabef1b1d8b76109ac467ee9f9ce1
Noel Muñoz <estebannoel98@hotmail.com>
2019-09-06 08:28:32 -0600

    commit 1

--- Cama.cpp
+++ Cama.cpp
@@ -3,4 +3,11 @@
 //
 
 #include "Cama.h"
+#include <iostream>
 
+Cama() {
+	setCodigo();
+	setEstado();
+}
+
+Cama(std::string codigo, bool estado) : codigo(codigo), estado(estado) {};
--- Pabellon.cpp
+++ Pabellon.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "Pabellon.h"
+#include <iostream>
+
+Pabellon::Pabellon() {
+	setId(' ');
+	setGenero(' ');
+	setTamano(0);
+}
+
+Pabellon::Pabellon(char id, char genero, int tamano): id(id), genero(genero), tamano(tamano){}
+
+Pabellon::Pabellon(int tamano) {
+	for (int pabellon = 0; pabellon < tamano; pabellon++) {
+	}
+}
+
+Pabellon::~Pabellon(){}
+
+//Metodos SET
+void Pabellon::setId(char id){
+	id = id;
+}
+void Pabellon::setGenero(char genero) {
+	genero = genero;
+}
+void Pabellon::setTamano(int tamano){
+	tamano = tamano;
+}
+void Pabellon::setCantidad(int cantidad){}
+
+//Metodos GET
+char Pabellon::getId() const{
+	return id;
+}
+char Pabellon::getGenero() const{
+	return genero;
+}
+int Pabellon::getTamano() const {
+	return tamano;
+}
+int Pabellon::getCantidad() const {
+	return cantidad;
+}
